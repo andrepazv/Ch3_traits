e4b386f1fd7eb280c123eaaeac29dc36bce037a1
Andrea Paz <paz.andreita@gmail.com>
2021-02-13 14:38:04 -0500

    Add files via upload

--- diversity_mapping_MPT.R
+++ diversity_mapping_MPT.R
@@ -31,7 +31,6 @@ rasterize_species= function (x,mask=selected_mask,r=raster_base) {
 ###load mask to use (AF) ##change path
 selected_mask<-readOGR("/Users/andreapaz/Dropbox/Guano's_files/AF_shapefile_from_Guano","atlantic_forest")
 selected_mask<-readOGR("D:\\Andrea\\Ch2","atlantic_forest")
-
 selected_mask = aggregate(selected_mask, dissolve = TRUE)
 ###list species
 setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/Points_only/") ##to folder with points
@@ -51,6 +50,7 @@ layers[sapply(layers,    is.null)]<-NULL
 Stack_points<-stack(layers)
 ###load SDMs and make correct size
 setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/OR_AUC/OR_AUC_T10/") ##to folder with SDMs
+
 models<-list.files()
 for(i in 1:length(models)){
   r<-raster(models[i])
@@ -65,16 +65,15 @@ for(i in 1:length(models)){
   writeRaster(r,paste("AF_",models[i],sep=""),format="ascii")
 }
 ##when everything is created just:
-setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/OR_AUC/OR_AUC_T10/")
-setwd("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\OR_AUC_T10\\")
+#setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/OR_AUC/OR_AUC_T10/")
+setwd("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\OR_AUC_MPT//")
 models<-list.files(pattern="AF_")
 Stack_models<-stack(models)
-species_names_models<-sub("_OR_AUC_T10","",models) 
+species_names_models<-sub("_OR_AUC_MPT","",models) 
 species_names_models<-sub("AF_","",species_names_models)
 species_names_models<-sub(".asc","",species_names_models,fixed=T)
-setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/Points_only/") 
-setwd("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\Points_only\\")
-
+#setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/Points_only/") 
+setwd("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\Points_only/")
 points<-list.files(pattern=".asc")
 Stack_points<-stack(points)
 species_names_points<-sub(".asc","",points,fixed=T) 
@@ -87,14 +86,14 @@ TD<-calc(Stack_full,sum,na.rm=T)
 TD<-crop(TD,selected_mask)
 TD<-mask(TD,selected_mask)
 plot(TD)
-writeRaster(TD,"richness_hylids_complete_0_0083.asc")
+writeRaster(TD,"richness_hylids_complete_0_0083_MPT.asc")
 
 #### Phylogenetic Diversity
 
 ####Load Phylogeny, generate list of species
 ##To read a phylogeny in newick format use read.tree instead of read.nexus
 #In working directory always a file with the same name: phylogeny.nex if not then change name here
-setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/PD_data/")
+setwd("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap")
 ##read phylogeny
 user_phylogeny<-ape::read.tree("Jetz_Pyron_amph_shl_new_Consensus_7238.tre")
 
@@ -143,7 +142,7 @@ species_names<-colnames(community_data1)[2:length(community_data1)] #Store speci
 
 #Store 
 setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/Analyses2021/")
-write.table(community_data1, file = "communities_hylids.txt", append = FALSE,row.names=F,quote=F,sep="\t") #tratar de agregar nombre de mascara
+write.table(community_data1, file = "communities_hylidsMPT.txt", append = FALSE,row.names=F,quote=F,sep="\t") #tratar de agregar nombre de mascara
 
 community_data1[is.na(community_data1)] = 0
 #In the community data frame NA must be eliminated done before is this if you load community data and not maps?
@@ -161,7 +160,7 @@ pd.result <-picante::pd(community_data1[,2:ncol(community_data1)],pruned.tree,in
 community_data1$pd<-pd.result[[1]]
 
 #Write the new matrix to a file to avoid rerunning the script for potential further analyses
-write.table(community_data, file = "communities_hylids_and_pd.txt", append = FALSE,row.names=F,quote=F,sep="\t")
+write.table(community_data1, file = "communities_hylids_and_pd_MPT.txt", append = FALSE,row.names=F,quote=F,sep="\t")
 
 #Generate a raster containing PD information per pixel
 
@@ -177,7 +176,7 @@ pd_ras[community_data1$grilla]<-community_data1$pd
 
 #3- Save raster to file 
 
-writeRaster(pd_ras,"hylids_PD.asc",format="ascii")
+writeRaster(pd_ras,"hylids_PD_MPT.asc",format="ascii")
 
 #4-Optional plotting map in R 
 plot(pd_ras)
@@ -193,98 +192,6 @@ plot(pd_ras)
 library(geometry)
 library(ape)
 library(ade4)
-library(FD)
-FDind=function(trait,abund) {
-  # T = number of traits
-  T=dim(trait)[2]
-  # c = number of communities
-  C=dim(abund)[1]
-  # check coherence of number of species in 'traits' and 'abundances'
-  if (dim(abund)[2]!=dim(trait)[1]) stop(" Error : different number of species in 'trait' and 'abund' matrices ")
-  # check format of traits values
-  if (ncol(trait)<2) stop ("'Trait' must have at least 2 columns")
-  if (is.numeric(trait)==F) stop ("Traits values must be numeric")
-  # check absence of NA in 'traits'
-  if (length(which(is.na(trait)==T))!=0) stop(" Error : NA in 'trait' matrix ")
-  # replacement of NA in 'abund' by '0'
-  abund[which(is.na(abund))]=0
-  # definition of vector for results, with communities'names as given in 'abund'
-  Nbsp=rep(NA,C) ; names(Nbsp)=row.names(abund)
-  FRic=rep(NA,C) ; names(FRic)=row.names(abund)
-  FEve=rep(NA,C) ; names(FEve)=row.names(abund)
-  FDiv=rep(NA,C) ; names(FDiv)=row.names(abund)
-  FSpe=rep(NA,C) ; names(FSpe)=row.names(abund)
-  # scaling and centering of each trait according to all species values
-  traitCS=scale(trait, center=TRUE, scale=TRUE)
-  # functional specialization of each species (distance to point 0,0 in the standardized functional space)
-  FSpeS=(apply(traitCS, 1, function(x) {x%*%x}))^0.5
-  # loop to compute FRic, FEve, FDiv and FSpe on each community
-  for (i in 1:C){
-    # selection of species present in the community
-    esppres=which(abund[i,]>0)
-    #  number of species in the community
-    S=length(esppres) ; Nbsp[i]=S
-    # check if more species than traits
-    # if (S<=T) stop(paste("Number of species must be higher than number of traits in community:",row.names(abund)[i]))
-    # filter on 'trait' and 'abund' to keep only values of species present in the community
-    tr=traitCS[esppres,] ; ab=as.matrix(abund[i,esppres])
-    # scaling of abundances
-    abondrel=ab/sum(ab)
-    # Functional Diversity Indices
-    # FRic
-    # Using convhulln function
-    # volume
-    FRic[i]=round(convhulln(tr,"FA")$vol,6)
-    FD_dis[i]<-FD::gowdis(tr)
-    FDDis<-fdisp(FD_dis,marco)
-    # identity of vertices
-    vert0=convhulln(tr,"Fx TO 'vert.txt'")
-    vert1=scan("vert.txt",quiet=T)
-    vert2=vert1+1
-    vertices=vert2[-1]
-    # FEve
-    # computation of inter-species euclidian distances
-    distT=dist(tr, method="euclidian")
-    # computation of Minimum Spanning Tree and conversion of the 'mst' matrix into 'dist' class
-    linkmst=mst(distT) ; mstvect=as.dist(linkmst)
-    # computation of the pairwise cumulative relative abundances and conversion into 'dist' class
-    abond2=matrix(0,nrow=S,ncol=S)
-    for (q in 1:S)
-      for (r in 1:S)
-        abond2[q,r]=abondrel[q]+abondrel[r]
-    abond2vect=as.dist(abond2)  # end of q,r
-    # computation of weighted evenness (EW) for the (S-1) branches to link S species
-    EW=rep(0,S-1)
-    flag=1
-    for (m in 1:((S-1)*S/2)){if (mstvect[m]!=0) {EW[flag]=distT[m]/(abond2vect[m]) ; flag=flag+1}}  # end of m
-    # computation of the partial weighted evenness (PEW) and comparison with 1/S-1, and computation of FEve
-    minPEW=-rep(0,S-1) ; OdSmO=1/(S-1)
-    for (l in 1:(S-1))
-      minPEW[l]=min((EW[l]/sum(EW)), OdSmO)  # end of l
-    FEve[i]=round(((sum(minPEW))- OdSmO)/(1-OdSmO),6)
-    # FDiv
-    # traits values of vertices of the convex hull
-    trvertices=tr[vertices,]
-    # coordinates of the center of gravity of the vertices (Gv) of the convex hull
-    baryv=apply(trvertices,2,mean)
-    # euclidian distances to Gv (dB) of each of S species (centro de gravedad)
-    distbaryv=rep(0,S)
-    for (j in 1:S)
-      distbaryv[j]=(sum((tr[j,]-baryv)^2) )^0.5  # end of j
-    # mean euclidian distance to the center of gravity of the S species (i.e. mean of dB values)  (mean centro gravedad)
-    # bigger effect if you are very abundant (andrea)
-    meandB=mean(distbaryv)
-    # deviation of each species dB from mean dB
-    devdB=distbaryv-meandB
-    # relative abundances-weighted mean deviation
-    abdev=abondrel*devdB
-    # relative abundances-weighted mean of absolute deviations
-    ababsdev=abondrel*abs(devdB)
-  } # end of i
-  # result storage
-  res=data.frame(Nbsp=Nbsp, FRic=FRic, FDis) ; row.names(res)=row.names(abund)
-  invisible(res)
-}# end of function
 
 ##Load maps exactly as for PD if not yet loaded
 setwd("~/Dropbox/**Tesis_PHD/Ch2_traits/SDM/OR_AUC/OR_AUC_T10/")
@@ -312,7 +219,7 @@ Stack_full<-stack(Stack_points,Stack_models)
 
 #Trait data: select file containing trait data
 trait<-read.csv("~/Dropbox/**Tesis_PHD/Ch2_traits/Analyses2021/Species_traits_imputed_lilian.csv",h=T)
-trait<-read.csv("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\Species_traits_imputed_lilian.csv")
+trait<-read.csv("Species_traits_imputed_lilian.csv")
 ###Trim trait data to match distribution data #2 don t match the ocean ones
 trait<-trait[trait$Species %in% species_names_full,]
 #trait<-na.omit(trait)
@@ -350,10 +257,10 @@ marco<-marco[-which(rowSums(marco[,2:159])==0),]
 
 
 
-
+trait1<-trait
 ###compute Functional dispersion including categorical data
 #Trait data: select file containing trait data
-trait1<-read.csv("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\Species_traits_imputed_lilian.csv")
+trait1<-read.csv("~/Dropbox/**Tesis_PHD/Ch2_traits/Analyses2021/Species_traits_imputed_lilian.csv",h=T)
 #FD_dis<-FD::gowdis(trait1[,c(3,4,7,8)])
 #FDDis<-FD::fdisp(FD_dis,marco)
 ##Trim trait data to match distribution data #2 don t match the ocean ones
@@ -380,7 +287,7 @@ writeRaster(fdric_ras,"hylid_functional_richness.asc", format="ascii")
 
 ###try with categorical
 
-trait2<-read.csv("C:\\Users\\AnaCarnaval\\Dropbox\\Andrea_Lab\\trasnfer_819_PC\\Functional_chap\\Species_traits_imputed_lilian.csv")
+trait2<-read.csv("~/Dropbox/**Tesis_PHD/Ch2_traits/Analyses2021/Species_traits_imputed_lilian.csv",h=T)
 rownames(trait2)<-trait2[,1]
 trait2<-trait2[,c(3,4,7,8)] #rep mode, size, head w tibiaL
 for (i in 2:4)
@@ -389,17 +296,79 @@ for (i in 2:4)
 }
 ##check if cat is cat
 FDindices_cat<-FD::dbFD(trait2,marco[,2:159],calc.FDiv=F,corr="cailliez")
+marco$fdDis<-FDDis[,2] ##check
+###prob remove
 
-map<-raster(Stack_full[[1]])
-fddisC_ras<-r
-fdricC_ras<-r
-values(fddisC_ras)<-NA # Erase alll values from the distribution map
-values(fdricC_ras)<-NA
-#Assign to the empty raster the values of FD that correspond to each pixel
-marco$fdDisC<-FDindices_cat$FDis
-marco$fdRicC<-FDindices_cat$FRic
-fddisC_ras[marco$grilla]<-marco$fdDisC
-fdricC_ras[marco$grilla]<-marco$fdRicC
-writeRaster(fddisC_ras,"hylid_functional_dispersion_cat1k.asc", format="ascii")
-writeRaster(fdricC_ras,"hylid_functional_richness_cat1k.asc", format="ascii")
-write.table(marco, file = "communities_hylids_and_fd10k_cat.txt", append = FALSE,row.names=F,quote=F,sep="\t")
+# Select traits 
+##remove non trait info like typo of model , for richness remove the categorical rep mode
+##remove dpulicted size column using data from Celio , keep size, HW and TL
+trait<-trait[,c(1,4,7,8)]
+str(trait)
+head(trait)
+summary(trait)
+traitnb=dim(trait)[2]-1 #Number of traits, minus 1 because the first column contains species names
+spnb=dim(trait)[1] #Number of species (one species per row thus number of species=number of rows)
+### log transform traits
+
+for (i in 2:traitnb)
+{
+  trait[,i]<-log(trait[,i]) ## This could be updated to another mathematic transformation if needed
+}
+
+
+traitc=trait[,2:traitnb] #Select only columns containign trait info and not species names
+traitcn=apply(traitc,2,as.numeric) #Ensure all trait values are numeric
+
+
+###Confirm number of species with map=number of species with traits
+####Confirm names of species with map are the same as names of species with traits
+trait[,1]<-as.character(trait[,1])
+differences<-setdiff(trait[,1],species_names)
+
+# Calculate FRic, FEve, FDiv and FSpe
+if (length(differences)==0){
+  FD=as.numeric() #Create an empty vector to store functional diversity data
+  # Community=unique(marco$grilla)
+  FD.i=FDind(traitcn,marco[,1:spnb])
+  for (i in 1:length(Community)) {
+    
+    abund.i=marco[marco$grilla==Community[i],1:spnb]
+    if ((sum(abund.i)-abund.i[1])>=5){
+      abundm.i=as.matrix(abund.i)
+      
+      FD.i=FDind(traitcn,abundm.i)
+      FD=rbind(FD,FD.i)
+    }
+    else 
+    {    FD.i=data.frame(Nbsp=NA, FRic=NA, FEve=NA, FDiv=NA, FSpe=NA) ; row.names(FD.i)=row.names(abund.i)
+    FD=rbind(FD,FD.i)
+    }
+  }
+  
+  marco$fd<-FD[,2]
+  map<-raster(Stack_full[[1]])
+  fd_ras<-r
+  values(fd_ras)<-NA # Erase alll values from the distribution map
+  
+  #Assign to the empty raster the values of FD that correspond to each pixel
+  fd_ras[marco$grilla]<-marco$fd
+  
+  
+  #Compute taxonomic diversity as number of species done before
+  #  richness<-stack(layers)
+  # TD<-calc(richness,sum,na.rm=T)
+  
+  
+  #Plot both maps in R to the left functional diversity and to the right taxonomic diversity
+  # par(mfrow=c(1,2))
+  
+  #plot(fd_ras, main="Functional Diversity")
+  #  plot(TD, main="Species Richness")
+  
+  #Write rasters (FD and TD) to file
+  setwd("~/")
+  writeRaster(fd_ras,"hylid_functional_richness.asc", format="ascii")
+  
+  
+} else print("There are differences in the species names in the trait and distriubtion databases:");differences
+
